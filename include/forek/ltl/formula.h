7cfd2ae3e06f9ecd460574e5901c88fafa1bfc57
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-03 14:20:06 -0700

    feat: Finish implementation of LTL Formula structure

--- include/forek/ltl/formula.h
+++ include/forek/ltl/formula.h
@@ -2,10 +2,25 @@
 
 #include <string>
 
+#include "forek/ltl/tree.h"
+#include "forek/ltl/visitor.h"
+
 namespace forek::ltl {
 class Formula {
+   private:
+    std::shared_ptr<Tree> m_root;
+
    public:
+    Formula() = delete;
+    Formula(const Formula& other) = default;
+    Formula(Formula&& other) = default;
+
     explicit Formula(std::string formula);
+
+    template <typename T>
+    auto evaluate(Visitor<T>& v) {
+        return m_root->visit(v);
+    }
 };
 
 class PastTimeFormula {
