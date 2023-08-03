7cccac0160a74d4936c38814dd44153a76f00402
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-02 21:39:12 -0700

    style: Format pl source file

--- src/pl.cxx
+++ src/pl.cxx
@@ -1,27 +1,28 @@
 #include <string>
 
 #include "forek/ir.h"
-#include "forek/pl/tree.h"
 #include "forek/pl/formula.h"
+#include "forek/pl/tree.h"
+
 #include "ANTLRInputStream.h"
 #include "CommonTokenStream.h"
 #include "PropositionalLogicLexer.h"
 #include "PropositionalLogicParser.h"
 #include "PropositionalLogicParserVisitor.h"
 
-using forek::pl::Formula;
-using forek::ir::True;
-using forek::ir::False;
-using forek::ir::Proposition;
-using forek::ir::Negation;
-using forek::ir::Conjunction;
-using forek::ir::Disjunction;
-using forek::ir::Implication;
-using forek::ir::Equivalence;
-using forek::pl::Tree;
 using forek::PropositionalLogicLexer;
 using forek::PropositionalLogicParser;
 using forek::PropositionalLogicParserVisitor;
+using forek::ir::Conjunction;
+using forek::ir::Disjunction;
+using forek::ir::Equivalence;
+using forek::ir::False;
+using forek::ir::Implication;
+using forek::ir::Negation;
+using forek::ir::Proposition;
+using forek::ir::True;
+using forek::pl::Formula;
+using forek::pl::Tree;
 
 class FormulaBuilder : public PropositionalLogicParserVisitor {
     auto visitStart(PropositionalLogicParser::StartContext *ctx) -> std::any override {
@@ -33,63 +34,67 @@ class FormulaBuilder : public PropositionalLogicParserVisitor {
     }
 
     auto visitPlTrue(PropositionalLogicParser::PlTrueContext *ctx) -> std::any override {
-        return Tree {True {}};
+        return Tree{True{}};
     }
 
     auto visitPlFalse(PropositionalLogicParser::PlFalseContext *ctx) -> std::any override {
-        return Tree {False {}};
+        return Tree{False{}};
     }
-    
+
     auto visitProposition(PropositionalLogicParser::PropositionContext *ctx) -> std::any override {
         auto name = ctx->Identifier()->getText();
-        auto prop = Proposition {std::move(name)};
+        auto prop = Proposition{std::move(name)};
 
-        return Tree {std::move(prop)};
+        return Tree{std::move(prop)};
     }
 
-    auto visitPlProposition(PropositionalLogicParser::PlPropositionContext *ctx) -> std::any override {
+    auto visitPlProposition(PropositionalLogicParser::PlPropositionContext *ctx)
+        -> std::any override {
         return visit(ctx->proposition());
     }
 
     auto visitPlNegation(PropositionalLogicParser::PlNegationContext *ctx) -> std::any override {
         auto inner = visit(ctx->formula());
         auto subtree = std::any_cast<Tree>(inner);
-        auto neg = Negation {std::move(subtree)};
+        auto neg = Negation{std::move(subtree)};
 
-        return Tree {std::move(neg)};
+        return Tree{std::move(neg)};
     }
 
-    auto visitPlConjunction(PropositionalLogicParser::PlConjunctionContext *ctx) -> std::any override {
+    auto visitPlConjunction(PropositionalLogicParser::PlConjunctionContext *ctx)
+        -> std::any override {
         auto left = visit(ctx->formula(0));
         auto right = visit(ctx->formula(1));
 
         auto left_subtree = std::any_cast<Tree>(left);
         auto right_subtree = std::any_cast<Tree>(right);
-        auto conj = Conjunction {std::move(left_subtree), std::move(right_subtree)};
-        
-        return Tree {std::move(conj)};
+        auto conj = Conjunction{std::move(left_subtree), std::move(right_subtree)};
+
+        return Tree{std::move(conj)};
     }
 
-    auto visitPlDisjunction(PropositionalLogicParser::PlDisjunctionContext *ctx) -> std::any override {
+    auto visitPlDisjunction(PropositionalLogicParser::PlDisjunctionContext *ctx)
+        -> std::any override {
         auto left = visit(ctx->formula(0));
         auto right = visit(ctx->formula(1));
 
         auto left_subtree = std::any_cast<Tree>(left);
         auto right_subtree = std::any_cast<Tree>(right);
-        auto conj = Disjunction {std::move(left_subtree), std::move(right_subtree)};
-        
-        return Tree {std::move(conj)};
+        auto conj = Disjunction{std::move(left_subtree), std::move(right_subtree)};
+
+        return Tree{std::move(conj)};
     }
 
-    auto visitPlImplication(PropositionalLogicParser::PlImplicationContext *ctx) -> std::any override {
+    auto visitPlImplication(PropositionalLogicParser::PlImplicationContext *ctx)
+        -> std::any override {
         auto left = visit(ctx->formula(0));
         auto right = visit(ctx->formula(1));
 
         auto left_subtree = std::any_cast<Tree>(left);
         auto right_subtree = std::any_cast<Tree>(right);
-        auto conj = Implication {std::move(left_subtree), std::move(right_subtree)};
-        
-        return Tree {std::move(conj)};
+        auto conj = Implication{std::move(left_subtree), std::move(right_subtree)};
+
+        return Tree{std::move(conj)};
     }
 
     auto visitPlIff(PropositionalLogicParser::PlIffContext *ctx) -> std::any override {
@@ -98,9 +103,9 @@ class FormulaBuilder : public PropositionalLogicParserVisitor {
 
         auto left_subtree = std::any_cast<Tree>(left);
         auto right_subtree = std::any_cast<Tree>(right);
-        auto conj = Equivalence {std::move(left_subtree), std::move(right_subtree)};
-        
-        return Tree {std::move(conj)};
+        auto conj = Equivalence{std::move(left_subtree), std::move(right_subtree)};
+
+        return Tree{std::move(conj)};
     }
 };
 
@@ -109,7 +114,7 @@ Formula::Formula(std::string formula) {
     auto lexer = PropositionalLogicLexer(&input_stream);
     auto token_stream = antlr4::CommonTokenStream(&lexer);
     auto parser = PropositionalLogicParser(&token_stream);
-    auto builder = FormulaBuilder {};
+    auto builder = FormulaBuilder{};
     auto output = builder.visit(parser.start());
     auto root = std::any_cast<Tree>(output);
 
