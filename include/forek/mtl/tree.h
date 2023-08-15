9c575a9efa5e1105ef8b59811c12674f01efe7ad
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-15 15:32:44 -0700

    feat: Define MTL tree

---
+++ include/forek/mtl/tree.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <variant>
+
+#include "forek/ir.h"
+#include "forek/mtl/visitor.h"
+
+namespace forek::mtl {
+class Tree {
+   private:
+    using Node = std::variant<ir::True, ir::False, ir::Proposition, ir::Negation<Tree>,
+                              ir::Conjunction<Tree>, ir::Disjunction<Tree>, ir::Implication<Tree>,
+                              ir::Equivalence<Tree>, ir::Globally<Tree>, ir::Finally<Tree>,
+                              ir::Next<Tree>, ir::Release<Tree>, ir::Until<Tree>,
+                              ir::BoundedGlobally<Tree>, ir::BoundedFinally<Tree>, ir::BoundedRelease<Tree>, ir::BoundedUntil<Tree> >;
+
+    Node m_inner;
+
+   public:
+    Tree() = delete;
+    Tree(const Tree&) = default;
+    Tree(Tree&&) = default;
+
+    explicit Tree(Node node) : m_inner{std::move(node)} {}
+
+    template <typename T>
+    auto visit(Visitor<T>& v) -> T {
+        std::visit([&v](auto inner) { return inner.visit(v); }, m_inner);
+    }
+};
+}  // namespace forek::mtl
