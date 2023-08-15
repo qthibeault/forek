31ce7f870e238ae9efc336f78076aefcc8b98276
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-15 15:00:29 -0700

    feat: Define virtual methods for mtl visitor

--- include/forek/mtl/visitor.h
+++ include/forek/mtl/visitor.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #include "forek/ltl/visitor.h"
+#include "forek/interval.h"
 
 namespace forek::mtl {
 template <typename T>
 class Visitor : public virtual ltl::Visitor<T> {
    public:
     virtual ~Visitor() = default;
+    virtual auto visit_bounded_globally(const interval::Interval&, T) -> T;
+    virtual auto visit_bounded_finally(const interval::Interval&, T) -> T;
+    virtual auto visit_bounded_release(const interval::Interval&, T) -> T;
+    virtual auto visit_bounded_until(const interval::Interval&, T) -> T;
 };
 }  // namespace forek::mtl
