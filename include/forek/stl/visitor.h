cd70d5499f73a39d06b4569368313f1f540ba290
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-24 15:09:25 -0700

    feat: Define methods in STL Visitor interface

--- include/forek/stl/visitor.h
+++ include/forek/stl/visitor.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <span>
+
 #include "forek/mtl/visitor.h"
 
 namespace forek::stl {
 template <typename T>
 class Visitor : public virtual mtl::Visitor<T> {
    public:
+    Visitor() = default;
+    Visitor(const Visitor&) = default;
+    Visitor(Visitor&&) = default;
+
+    auto operator=(const Visitor&) -> Visitor& = default;
+    auto operator=(Visitor&&) -> Visitor& = default;
+
+    auto visit_predicate() -> T = 0;
+
     virtual ~Visitor() = default;
 };
 }  // namespace forek::stl
