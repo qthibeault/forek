e7816cbf8b330ad6cf1ab8daed3ed15ce66ec659
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-24 15:07:16 -0700

    feat: Complete definition for STL Formula

--- include/forek/stl/formula.h
+++ include/forek/stl/formula.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <memory>
 #include <string>
+
+#include "forek/stl/tree.h"
+#include "forek/stl/visitor.h"
 
 namespace forek::stl {
 class Formula {
+    std::shared_ptr<Tree> m_root;
+    
    public:
     explicit Formula(std::string formula);
+    explicit Formula(Tree tree);
+    explicit Formula(std::shared_ptr<Tree> tree);
+
+    template <typename T>
+    auto evalute(Visitor<T>& v) -> T {
+        return m_root->accept(v);
+    }
 };
 }  // namespace forek::stl
