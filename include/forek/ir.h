08d650d7090b151bf2487f032117d692948fe2b8
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-24 13:55:34 -0700

    feat: Define Predicate type that contains a two sets of terms combined by an inequality operator

--- include/forek/ir.h
+++ include/forek/ir.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <memory>
+#include <optional>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "forek/interval.h"
 
@@ -30,6 +32,30 @@ struct Proposition {
     auto accept(V &visitor) const {
         return visitor.visit_proposition(m_name);
     }
+};
+
+struct Predicate {
+    class Term {
+        double m_scalar;
+        std::optional<std::string> m_name;
+
+       public:
+        Term(std::string name, double scalar) : m_scalar{scalar}, m_name{std::move(name)} {}
+        Term(double scalar, std::string name) : m_scalar{scalar}, m_name{std::move(name)} {}
+        explicit Term(double scalar) : m_scalar{scalar}, m_name{std::nullopt} {}
+
+        auto scalar() -> double { return m_scalar; }
+        auto name() -> std::optional<std::string> & { return m_name; }
+    };
+
+    enum class Comparison { GreaterThan, GreaterThenEquals, LessThan, LessThanEquals };
+
+    std::vector<Term> m_left;
+    Comparison m_cmp;
+    std::vector<Term> m_right;
+
+    Predicate(std::vector<Term> left, Comparison cmp, std::vector<Term> right)
+        : m_left{std::move(left)}, m_cmp{cmp}, m_right{std::move(right)} {}
 };
 
 template <typename Subtree>
