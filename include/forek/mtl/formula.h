8336f1031ab1e1675a5e4bd38efc0be82d0a8492
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-24 13:21:28 -0700

    feat: Finish mtl Formula definition

--- include/forek/mtl/formula.h
+++ include/forek/mtl/formula.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <memory>
 #include <string>
+
+#include "forek/mtl/tree.h"
+#include "forek/mtl/visitor.h"
 
 namespace forek::mtl {
 class Formula {
+    std::shared_ptr<Tree> m_root;
+
    public:
     explicit Formula(std::string formula);
+    explicit Formula(Tree root);
+
+    template <typename T>
+    auto evaluate(Visitor<T>& v) -> T {
+        return m_root->accept(v);
+    }
 };
 }  // namespace forek::mtl
