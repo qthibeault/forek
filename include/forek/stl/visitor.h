513929aa905b953fd954276a5885fe1de31cf4e3
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-26 16:22:45 -0700

    refactor: Update STL visitor method to accept expression trees instead of sums

--- include/forek/stl/visitor.h
+++ include/forek/stl/visitor.h
@@ -2,11 +2,15 @@
 
 #include <span>
 
+#include "forek/algebra.h"
 #include "forek/mtl/visitor.h"
 
 namespace forek::stl {
 template <typename T>
 class Visitor : public virtual mtl::Visitor<T> {
+    using Expr = algebra::Expr;
+    using Comparison = algebra::Comparison;
+
    public:
     Visitor() = default;
     Visitor(const Visitor&) = default;
@@ -15,7 +19,7 @@ class Visitor : public virtual mtl::Visitor<T> {
     auto operator=(const Visitor&) -> Visitor& = default;
     auto operator=(Visitor&&) -> Visitor& = default;
 
-    auto visit_predicate() -> T = 0;
+    auto visit_predicate(const Expr& lhs, Comparison cmp, const Expr& rhs) -> T = 0;
 
     virtual ~Visitor() = default;
 };
