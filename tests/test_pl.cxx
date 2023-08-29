ae6fe6831052e5b4b17e6b7266134c0ca5ea0795
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-29 11:43:31 -0700

    test: Refactor propositional logic tests to test supported operators

--- tests/test_pl.cxx
+++ tests/test_pl.cxx
@@ -1,95 +1,49 @@
 #include "forek/pl.h"
+#include "forek/errors.h"
 
 #include <exception>
 #include <string>
 
 #include <catch2/catch_test_macros.hpp>
 
+using forek::errors::LexerError;
+using forek::errors::ParserError;
 using forek::pl::Formula;
 
-auto try_parse(std::string formula) -> bool {
-    try {
-        Formula(std::move(formula));
-        return true;
-    } catch (std::exception& ex) {
-        return false;
-    }
-}
-
 TEST_CASE("Propositional Logic", "[pl]") {
-    SECTION("Requirement 1") {
-        static std::string req =
-            "p";
-
-        REQUIRE(try_parse(req));
+    SECTION("Proposition") {
+        REQUIRE_NOTHROW(Formula("p"));
+        REQUIRE_NOTHROW(Formula("p_1"));
+        REQUIRE_THROWS_AS(Formula("1p"), ParserError);
+        REQUIRE_THROWS_AS(Formula("~p"), LexerError);
     }
 
-    SECTION("Requirement 2") {
-        static std::string req =
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q)";
-
-        REQUIRE(try_parse(req));
+    SECTION("Negation") {
+        REQUIRE_NOTHROW(Formula("!p"));
+        REQUIRE_NOTHROW(Formula("not p"));
     }
 
-    SECTION("Requirement 3") {
-        static std::string req =
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) && "
-            "not (p) && (not q) && not (p) && (not q)";
-
-        REQUIRE(try_parse(req));
+    SECTION("Conjunction") {
+        REQUIRE_NOTHROW(Formula("p && q"));
+        REQUIRE_NOTHROW(Formula("p and q"));
     }
 
-    SECTION("Requirement 4") {
-        static std::string req =
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) or not (p) && (not q)";
-
-        REQUIRE(try_parse(req));
+    SECTION("Disjunction") {
+        REQUIRE_NOTHROW(Formula("p || q"));
+        REQUIRE_NOTHROW(Formula("p or q"));
     }
 
-    SECTION("Requirement 5") {
-        static std::string req =
-            "not (p) && (not q) & not (p) && (not q) & not (p) && (not q) or not (p) && (not q) and "
-            "not (p) && (not q)";
-
-        REQUIRE(try_parse(req));
+    SECTION("Implication") {
+        REQUIRE_NOTHROW(Formula("p -> q"));
+        REQUIRE_NOTHROW(Formula("p implies q"));
     }
 
-    SECTION("Requirement 6") {
-        static std::string req =
-            "not (p) && (not q) && not (p) && (not q) && not (p) && (not q) or not (p) && (not q) and "
-            "not (p) && (not q) implies not (p) && (not q)";
+    SECTION("Equivalence") {
+        REQUIRE_NOTHROW(Formula("p <-> q"));
+        REQUIRE_NOTHROW(Formula("p iff q"));
+    }
 
-        REQUIRE(try_parse(req));
+    SECTION("Composition") {
+        REQUIRE_NOTHROW(Formula("not a and (b -> c) or (!d <-> e)"));
     }
 }
