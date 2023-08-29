ac10cff21c56756e76a025d589b026f5f1020832
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-29 15:31:00 -0700

    test: Add MTL and STL formulas to propositional logic testing matrix

--- tests/test_pl.cxx
+++ tests/test_pl.cxx
@@ -13,38 +13,45 @@
 using forek::errors::LexerError;
 using forek::errors::ParserError;
 
-TEMPLATE_TEST_CASE("Proposition", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("Proposition", "[pl]", forek::pl::Formula, forek::ltl::Formula,
+                   forek::mtl::Formula, forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("p"));
     REQUIRE_NOTHROW(TestType("p_1"));
     REQUIRE_THROWS_AS(TestType("1p"), ParserError);
     REQUIRE_THROWS_AS(TestType("~p"), LexerError);
 }
 
-TEMPLATE_TEST_CASE("Negation", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("Negation", "[pl]", forek::pl::Formula, forek::ltl::Formula, forek::mtl::Formula,
+                   forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("!p"));
     REQUIRE_NOTHROW(TestType("not p"));
 }
 
-TEMPLATE_TEST_CASE("Conjunction", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("Conjunction", "[pl]", forek::pl::Formula, forek::ltl::Formula,
+                   forek::mtl::Formula, forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("p && q"));
     REQUIRE_NOTHROW(TestType("p and q"));
 }
 
-TEMPLATE_TEST_CASE("Disjunction", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("Disjunction", "[pl]", forek::pl::Formula, forek::ltl::Formula,
+                   forek::mtl::Formula, forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("p || q"));
     REQUIRE_NOTHROW(TestType("p or q"));
 }
 
-TEMPLATE_TEST_CASE("Implication", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("Implication", "[pl]", forek::pl::Formula, forek::ltl::Formula,
+                   forek::mtl::Formula, forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("p -> q"));
     REQUIRE_NOTHROW(TestType("p implies q"));
 }
 
-TEMPLATE_TEST_CASE("Equivalence", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("Equivalence", "[pl]", forek::pl::Formula, forek::ltl::Formula,
+                   forek::mtl::Formula, forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("p <-> q"));
     REQUIRE_NOTHROW(TestType("p iff q"));
 }
 
-TEMPLATE_TEST_CASE("PL Composition", "[pl]", forek::pl::Formula, forek::ltl::Formula) {
+TEMPLATE_TEST_CASE("PL Composition", "[pl]", forek::pl::Formula, forek::ltl::Formula,
+                   forek::mtl::Formula, forek::stl::Formula) {
     REQUIRE_NOTHROW(TestType("not a and (b -> c) or (!d <-> e)"));
 }
