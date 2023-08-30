11ebe7eae30a506df9f4444427fc8c553330bb97
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-30 13:20:50 -0700

    fix: Follow rule of 5 in pl logic visitor interface

--- include/forek/pl/visitor.h
+++ include/forek/pl/visitor.h
@@ -3,17 +3,26 @@
 #include <string>
 
 namespace forek::pl {
+
 template <typename T>
 class Visitor {
    public:
-    virtual auto visit_true() -> T;
-    virtual auto visit_false() -> T;
-    virtual auto visit_proposition(std::string name) -> T;
-    virtual auto visit_negation(T inner) -> T;
-    virtual auto visit_conjunction(T left, T right) -> T;
-    virtual auto visit_disjunction(T left, T right) -> T;
-    virtual auto visit_implication(T ante, T cons) -> T;
-    virtual auto visit_equivalence(T ante, T cons) -> T;
+    Visitor() = default;
+    Visitor(const Visitor&) = default;
+    Visitor(Visitor&&) = default;
+
+    auto operator=(const Visitor&) -> Visitor& = default;
+    auto operator=(Visitor&&) -> Visitor& = default;
+
+    virtual auto visit_true() -> T = 0;
+    virtual auto visit_false() -> T = 0;
+    virtual auto visit_proposition(std::string name) -> T = 0;
+    virtual auto visit_negation(T inner) -> T = 0;
+    virtual auto visit_conjunction(T left, T right) -> T = 0;
+    virtual auto visit_disjunction(T left, T right) -> T = 0;
+    virtual auto visit_implication(T ante, T cons) -> T = 0;
+    virtual auto visit_equivalence(T ante, T cons) -> T = 0;
     virtual ~Visitor() = default;
 };
+
 }  // namespace forek::pl
