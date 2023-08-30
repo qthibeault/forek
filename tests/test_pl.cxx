4944511eb72693ab97a27685e7eda47883fadcdb
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-08-30 15:03:54 -0700

    test: Create Catch2 StringMaker instance for propositional logic formula

--- tests/test_pl.cxx
+++ tests/test_pl.cxx
@@ -1,8 +1,8 @@
-#include <exception>
 #include <string>
 
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <fmt/format.h>
 
 #include "forek/errors.h"
 #include "forek/ltl.h"
@@ -12,6 +12,45 @@
 
 using forek::errors::LexerError;
 using forek::errors::ParserError;
+
+namespace Catch {
+
+template <>
+struct StringMaker<forek::pl::Formula> {
+    static auto convert(const forek::pl::Formula& formula) -> std::string {
+        class DebugRepr : public forek::pl::Visitor<std::string> {
+           public:
+            auto visit_true() -> std::string override { return "True"; }
+            auto visit_false() -> std::string override { return "False"; }
+            auto visit_proposition(std::string name) -> std::string override { return name; }
+
+            auto visit_negation(std::string inner) -> std::string override {
+                return fmt::format("Not({})", inner);
+            }
+
+            auto visit_conjunction(std::string left, std::string right) -> std::string override {
+                return fmt::format("And(left={} right={})", left, right);
+            }
+
+            auto visit_disjunction(std::string left, std::string right) -> std::string override {
+                return fmt::format("Or(left={} right={})", left, right);
+            }
+
+            auto visit_implication(std::string ante, std::string cons) -> std::string override {
+                return fmt::format("Impl(left={} right={})", ante, cons);
+            }
+
+            auto visit_equivalence(std::string ante, std::string cons) -> std::string override {
+                return fmt::format("Equiv(left={} right={})", ante, cons);
+            }
+        };
+
+        DebugRepr dr;
+        return formula.evaluate(dr);
+    }
+};
+
+}  // namespace Catch
 
 TEMPLATE_TEST_CASE("Proposition", "[pl]", forek::pl::Formula, forek::ltl::Formula,
                    forek::mtl::Formula, forek::stl::Formula) {
