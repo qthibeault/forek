61497a24c24a35f4afedae6c6d72a03c0c4609c5
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-09-05 21:27:31 -0700

    fix: Make MTL visitor abstract and define special member functions

--- include/forek/mtl/visitor.h
+++ include/forek/mtl/visitor.h
@@ -4,14 +4,26 @@
 #include "forek/ltl/visitor.h"
 
 namespace forek::mtl {
+
 template <typename T>
-class Visitor : public virtual ltl::Visitor<T> {
+class Visitor : public ltl::Visitor<T> {
+   private:
+    using Interval = interval::Interval;
+
    public:
+    Visitor() = default;
+    Visitor(const Visitor<T>&) = default;
+    Visitor(Visitor<T>&&) = default;
+
+    auto operator=(const Visitor<T>&) -> Visitor<T>& = default;
+    auto operator=(Visitor<T>&&) -> Visitor<T>& = default;
+
     virtual ~Visitor() = default;
-    virtual auto visit_bounded_next(const interval::Interval &, T) -> T = 0;
-    virtual auto visit_bounded_globally(const interval::Interval &, T) -> T = 0;
-    virtual auto visit_bounded_finally(const interval::Interval &, T) -> T = 0;
-    virtual auto visit_bounded_release(const interval::Interval &, T) -> T = 0;
-    virtual auto visit_bounded_until(const interval::Interval &, T) -> T = 0;
+    virtual auto visit_bounded_next(const Interval &, T) -> T = 0;
+    virtual auto visit_bounded_globally(const Interval &, T) -> T = 0;
+    virtual auto visit_bounded_finally(const Interval &, T) -> T = 0;
+    virtual auto visit_bounded_release(const Interval &, T, T) -> T = 0;
+    virtual auto visit_bounded_until(const Interval &, T, T) -> T = 0;
 };
+
 }  // namespace forek::mtl
