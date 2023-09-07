df59d1defadff83b271a4ff83ab3920e1a6e2d54
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-09-07 16:49:35 -0700

    refactor: Move interval method definitions to source file to avoid conflicts

--- include/forek/interval.h
+++ include/forek/interval.h
@@ -9,29 +9,33 @@
 namespace forek::interval {
 
 class Inclusive {
-    double m_value;
-
    public:
-    explicit Inclusive(double value) : m_value{value} {}
+    explicit Inclusive(double value);
 
-    [[nodiscard]] auto value() const noexcept -> double { return m_value; }
+    auto operator==(const Inclusive& lhs) const -> bool;
+
+    [[nodiscard]] auto value() const noexcept -> double;
+
+   private:
+    double m_value;
 };
 
 class Exclusive {
-    double m_value;
-
    public:
-    explicit Exclusive(double value) : m_value{value} {}
+    explicit Exclusive(double value);
 
-    [[nodiscard]] auto value() const noexcept -> double { return m_value; }
+    auto operator==(const Exclusive& lhs) const -> bool;
+
+    [[nodiscard]] auto value() const noexcept -> double;
+
+   private:
+    double m_value;
 };
 
 class Endpoint {
-    std::variant<Inclusive, Exclusive> m_inner;
-
    public:
-    Endpoint(Inclusive inner) : m_inner{inner} {}  // NOLINT(google-explicit-constructor)
-    Endpoint(Exclusive inner) : m_inner{inner} {}  // NOLINT(google-explicit-constructor)
+    Endpoint(Inclusive inner);  // NOLINT(google-explicit-constructor)
+    Endpoint(Exclusive inner);  // NOLINT(google-explicit-constructor)
 
     template <typename T>
     struct Visitor {
@@ -47,37 +51,41 @@ class Endpoint {
         virtual ~Visitor() = default;
     };
 
-    [[nodiscard]] auto value() const -> double {
-        return std::visit([](auto x) { return x.value(); }, m_inner);
-    }
+    auto operator==(const Endpoint& lhs) const -> bool;
+
+    [[nodiscard]] auto value() const -> double;
 
     template <typename T>
-    [[nodiscard]] auto visit(Visitor<T> &v) const -> T {
-        return std::visit([&v](auto inner) { return v(inner); }, m_inner);
-    }
+    [[nodiscard]] auto visit(Visitor<T> &v) const -> T;
+
+   private:
+    std::variant<Inclusive, Exclusive> m_inner;
+
 };
 
 class Interval {
-    Endpoint m_lower;
-    Endpoint m_upper;
-
    public:
     Interval(Endpoint start, Endpoint end);
 
     explicit operator std::string() const;
+    auto operator==(const Interval& lhs) const -> bool;
 
-    [[nodiscard]] auto lower() const noexcept -> Endpoint { return m_lower; }
-    [[nodiscard]] auto upper() const noexcept -> Endpoint { return m_upper; }
+    [[nodiscard]] auto lower() const noexcept -> Endpoint;
+    [[nodiscard]] auto upper() const noexcept -> Endpoint;
+
+   private:
+    Endpoint m_lower;
+    Endpoint m_upper;
 };
 
 class ZeroLengthInterval : std::exception {
-   private:
-    std::string m_msg;
-
    public:
     explicit ZeroLengthInterval(const Interval &interval);
 
     [[nodiscard]] auto what() const noexcept -> const char * override { return m_msg.data(); }
+
+   private:
+    std::string m_msg;
 };
 
 auto make_inclusive(double value) -> Endpoint;
--- src/interval.cxx
+++ src/interval.cxx
@@ -3,21 +3,43 @@
 #include <fmt/core.h>
 #include <fmt/format.h>
 
-using forek::interval::Endpoint;
-using forek::interval::Exclusive;
-using forek::interval::Inclusive;
-using forek::interval::Interval;
-using forek::interval::ZeroLengthInterval;
+namespace forek::interval {
 
-struct OpenSymbolVisitor : public Endpoint::Visitor<char> {
-    auto operator()(const Exclusive &) const -> char override { return '('; }
-    auto operator()(const Inclusive &) const -> char override { return '['; }
-};
+Inclusive::Inclusive(double value) : m_value{value} {}
 
-struct CloseSymbolVisitor : public Endpoint::Visitor<char> {
-    auto operator()(const Exclusive &) const -> char override { return ')'; }
-    auto operator()(const Inclusive &) const -> char override { return ']'; }
-};
+auto Inclusive::operator==(const Inclusive &lhs) const -> bool {
+    return m_value == lhs.m_value;
+}
+
+auto Inclusive::value() const noexcept -> double {
+    return m_value;
+}
+
+Exclusive::Exclusive(double value) : m_value{value} {}
+
+auto Exclusive::operator==(const Exclusive &lhs) const -> bool {
+    return m_value == lhs.m_value;
+}
+
+auto Exclusive::value() const noexcept -> double {
+    return m_value;
+}
+
+Endpoint::Endpoint(Inclusive inner) : m_inner{inner} {}  // NOLINT(google-explicit-constructor)
+Endpoint::Endpoint(Exclusive inner) : m_inner{inner} {}  // NOLINT(google-explicit-constructor)
+
+auto Endpoint::operator==(const Endpoint &lhs) const -> bool {
+    return m_inner == lhs.m_inner;
+}
+
+auto Endpoint::value() const -> double {
+    return std::visit([](auto x) { return x.value(); }, m_inner);
+}
+
+template <typename T>
+auto Endpoint::visit(Visitor<T> &v) const -> T {
+    return std::visit([&v](auto inner) { return v(inner); }, m_inner);
+}
 
 Interval::Interval(Endpoint lower, Endpoint upper)
     : m_lower{std::move(lower)}, m_upper{std::move(upper)} {
@@ -26,8 +48,55 @@ Interval::Interval(Endpoint lower, Endpoint upper)
     }
 }
 
-auto fmt::formatter<Interval>::format(const Interval &i, format_context &ctx)
+auto Interval::lower() const noexcept -> Endpoint {
+    return m_lower;
+}
+
+auto Interval::upper() const noexcept -> Endpoint {
+    return m_upper;
+}
+
+Interval::operator std::string() const {
+    return fmt::format("{}", *this);
+}
+
+auto Interval::operator==(const Interval &lhs) const -> bool {
+    return m_lower == lhs.m_lower && m_upper == lhs.m_upper;
+}
+
+ZeroLengthInterval::ZeroLengthInterval(const Interval &i)
+    : m_msg{fmt::format("Interval {} has a length of zero", i)} {}
+
+auto make_inclusive(double value) -> Endpoint {
+    return {Inclusive{value}};
+}
+
+auto make_exclusive(double value) -> Endpoint {
+    return {Exclusive{value}};
+}
+
+}  // namespace forek::interval
+
+namespace fmt {
+
+using forek::interval::Interval;
+
+auto formatter<Interval>::format(const Interval &i, format_context &ctx)
     -> format_context::iterator {
+    using forek::interval::Endpoint;
+    using forek::interval::Exclusive;
+    using forek::interval::Inclusive;
+
+    struct OpenSymbolVisitor : public Endpoint::Visitor<char> {
+        auto operator()(const Exclusive &) const -> char override { return '('; }
+        auto operator()(const Inclusive &) const -> char override { return '['; }
+    };
+
+    struct CloseSymbolVisitor : public Endpoint::Visitor<char> {
+        auto operator()(const Exclusive &) const -> char override { return ')'; }
+        auto operator()(const Inclusive &) const -> char override { return ']'; }
+    };
+
     auto open_visitor = OpenSymbolVisitor{};
     auto open = i.lower().visit(open_visitor);
     auto start = i.lower().value();
@@ -39,10 +108,4 @@ auto fmt::formatter<Interval>::format(const Interval &i, format_context &ctx)
     return fmt::format_to(ctx.out(), "{}{},{}{}", open, start, end, close);
 }
 
-Interval::operator std::string() const { return fmt::format("{}", *this); }
-
-ZeroLengthInterval::ZeroLengthInterval(const Interval &i)
-    : m_msg{fmt::format("Interval {} has a length of zero", i)} {}
-
-auto forek::interval::make_inclusive(double value) -> Endpoint { return {Inclusive{value}}; }
-auto forek::interval::make_exclusive(double value) -> Endpoint { return {Exclusive{value}}; }
+}  // namespace fmt
