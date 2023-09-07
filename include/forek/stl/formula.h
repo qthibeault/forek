e870afc8b05bd0d1eda1df2e647b7d8d2c80ad7c
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-09-07 13:05:56 -0700

    feat: Define STL constructor using string_view instead of string. Declare comparison and string conversion operators

--- include/forek/stl/formula.h
+++ include/forek/stl/formula.h
@@ -2,22 +2,28 @@
 
 #include <memory>
 #include <string>
+#include <string_view>
 
 #include "forek/stl/tree.h"
 #include "forek/stl/visitor.h"
 
 namespace forek::stl {
+
 class Formula {
     std::shared_ptr<Tree> m_root;
 
    public:
-    explicit Formula(std::string formula);
+    explicit Formula(std::string_view formula);
     explicit Formula(Tree tree);
     explicit Formula(std::shared_ptr<Tree> tree);
+
+    auto operator==(const Formula& lhs) -> bool;
+    explicit operator std::string();
 
     template <typename T>
     auto evaluate(Visitor<T>& v) const -> T {
         return m_root->accept(v);
     }
 };
+
 }  // namespace forek::stl
