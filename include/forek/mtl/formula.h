c293820d4fe68e7a91d7b807a4e802c8f874ac04
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-09-07 13:00:43 -0700

    feat: Define MTL formula constructor using string_view instead of string. Declare comparison and string conversion operators

--- include/forek/mtl/formula.h
+++ include/forek/mtl/formula.h
@@ -2,21 +2,27 @@
 
 #include <memory>
 #include <string>
+#include <string_view>
 
 #include "forek/mtl/tree.h"
 #include "forek/mtl/visitor.h"
 
 namespace forek::mtl {
+
 class Formula {
     std::shared_ptr<Tree> m_root;
 
    public:
-    explicit Formula(std::string formula);
+    explicit Formula(std::string_view formula);
     explicit Formula(Tree root);
+
+    auto operator==(const Formula& lhs) -> bool;
+    explicit operator std::string();
 
     template <typename T>
     auto evaluate(Visitor<T>& v) const -> T {
         return m_root->accept(v);
     }
 };
+
 }  // namespace forek::mtl
